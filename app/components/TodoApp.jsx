e6b1a47d5fc59f374488cf9d71596e3b468dedd4
Moufid Larbi <moufid4@gmail.com>
2017-02-15 13:16:37 -0500

    added todosearch

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
@@ -33,6 +34,7 @@ var TodoApp = React.createClass({
     var {todos} = this.state;
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
