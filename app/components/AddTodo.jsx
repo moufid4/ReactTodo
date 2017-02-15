9f9ba0cd01ba53feb16d90cb3b7230249624061a
Moufid Larbi <moufid4@gmail.com>
2017-02-15 12:45:37 -0500

    added form

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -1,11 +1,26 @@
 var React = require('react');
 
 var AddTodo = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var todo = this.refs.todo.value;
+    if (todo.length > 0) {
+      this.refs.todo.value = '';
+      this.props.onAddTodo(todo);
+    } else {
+      this.refs.todo.focus();
+    }
+  },
   render: function () {
     return (
-
+      <div>
+        <form ref="form" onSubmit={this.handleSubmit} className="addtodo-form">
+          <input type="text" ref="todo" placeholder="Enter your todo"/>
+          <button className="button expanded">Add</button>
+        </form>
+      </div>
     );
-  };
+  }
 });
 
 module.exports = AddTodo;
