eb2fc94630f6abaa16043a408a319316fa21e991
Moufid Larbi <moufid4@gmail.com>
2017-02-20 11:09:11 -0500

    added completedAt timestamp

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -26,7 +26,8 @@ var TodoApp = React.createClass({
           id: uuid(),
           text: text,
           completed: false,
-          createdAt: moment().unix()
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
@@ -35,6 +36,7 @@ var TodoApp = React.createClass({
       var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed? moment().unix():undefined;
       }
 
       return todo;
